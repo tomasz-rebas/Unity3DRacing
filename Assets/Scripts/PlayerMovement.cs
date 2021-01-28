86bac35641f898bcb73b62493b691df1c151491c
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2021-01-28 18:50:00 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 3f;
+
+    void Start()
+    {
+        Debug.Log("Hello Word!");
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, speed);
+    }
+}
---
+++ Assets/Scripts/UpdateCameraPosition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateCameraPosition : MonoBehaviour
+{
+    private GameObject player;
+    private Transform playerTransform;
+    public Transform cameraTransform;
+    public float yOffset = 1.14f;
+    public float zOffset = -4.39f;
+
+    void Start()
+    {
+         player = GameObject.Find("Player");
+         playerTransform = player.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        cameraTransform.position = new Vector3(
+            playerTransform.position.x,
+            playerTransform.position.y + yOffset,
+            playerTransform.position.z + zOffset
+        );
+    }
+}
